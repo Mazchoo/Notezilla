f0e71f5d208969733ecb19aba94bd8d2fceda054
Matthew Mitchell <mazchoo@gmail.com>
2026-06-17 19:35:32 +0700

    Changed front matter icon to standard symbol

--- src/frontend/src/components/editor/block.rs
+++ src/frontend/src/components/editor/block.rs
@@ -88,8 +88,8 @@ pub fn TitleBlockComponent(
                                 on:click=move |_| add_front_matter(&s, entry_id)
                             >
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                                    <line x1="12" y1="5" x2="12" y2="19"/>
-                                    <line x1="5" y1="12" x2="19" y2="12"/>
+                                    <path d="M8 3H7a2 2 0 0 0-2 2v5a2 2 0 0 1-2 2 2 2 0 0 1 2 2v5c0 1.1.9 2 2 2h1"/>
+                                    <path d="M16 21h1a2 2 0 0 0 2-2v-5c0-1.1.9-2 2-2a2 2 0 0 1-2-2V5a2 2 0 0 0-2-2h-1"/>
                                 </svg>
                             </button>
                         })
