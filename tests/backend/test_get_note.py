947a02f994de24cb2aa4e745e19509408b2c95dd
Matthew Mitchell <mazchoo@gmail.com>
2026-06-29 21:21:33 +0700

    Extended tests for different paths

--- tests/backend/test_get_note.py
+++ tests/backend/test_get_note.py
@@ -23,6 +23,24 @@ class TestGetNote:
         assert result["error"] is None
         mock_db.query_by_id.assert_called_once_with("2024/01/note.md")
 
+    def test_backslash_path_calls_normalised_path(self, mock_db):  # pylint: disable=redefined-outer-name
+        """Expect key to be universal to get the same result each time"""
+        result = get_note(path="2024\\01\\note.md")
+
+        mock_db.query_by_id.assert_called_once_with("2024/01/note.md")
+
+    def test_relative_path_calls_normalised_path(self, mock_db):  # pylint: disable=redefined-outer-name
+        """Expect key to be universal to get the same result each time"""
+        result = get_note(path="./2024/01/note.md")
+
+        mock_db.query_by_id.assert_called_once_with("2024/01/note.md")
+
+    def test_backtrack_path_calls_normalised_path(self, mock_db):  # pylint: disable=redefined-outer-name
+        """Expect key to be universal to get the same result each time"""
+        result = get_note(path="./2024/01/../01/note.md")
+
+        mock_db.query_by_id.assert_called_once_with("2024/01/note.md")
+
     def test_invalid_path_returns_error(self, mock_db):  # pylint: disable=redefined-outer-name
         """get_note returns an error when the path is outside the note folder."""
         result = get_note(path="../../../etc/passwd")
